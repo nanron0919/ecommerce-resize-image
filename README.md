3b4b369fb0a83c516ea64f2cf1e87d792e45df83
Norman Chen <normanchen@bananawhale.com>
2019-01-09 00:37:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# e-commerce Resize Image
+
+> CLI Tool to resize images for e-commerce sites
+
+## Installation
+
+* Nodejs
+
+> Run `npm i`
+
+## Command
+
+> Run `./resize -d [PATH]`
---
+++ settings.js
@@ -0,0 +1,26 @@
+module.exports = {
+    'udesign': [{
+        'width': 1000,
+        'height': 1000,
+    }, {
+        'width': 500,
+        'height': 500,
+    }],
+    'shopee-sml': [{
+        'width': 800,
+    }],
+    'shopee-idea': [{
+        'width': 800,
+    }],
+    'books.com': [{
+        'width': 1000,
+    }],
+    'momo': [{
+        'width': 1000,
+        'height': 1000,
+    }],
+    'sml_shop': [{
+        'width': 750,
+        'height': 750,
+    }],
+};
